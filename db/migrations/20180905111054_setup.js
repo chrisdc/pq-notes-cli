d232d67889207c1576754557a38a8a882c7fb086
Christopher Crouch <chrisdc@gmail.com>
2018-09-24 15:55:37 +0100

    Specify id column names

--- db/migrations/20180905111054_setup.js
+++ db/migrations/20180905111054_setup.js
@@ -3,7 +3,7 @@
 exports.up = function(knex, Promise) {
   return Promise.all([
     knex.schema.createTable('notes', function(table) {
-      table.increments();
+      table.increments('id');
       table.timestamps(false, true);
       table.string('name').notNullable();
       table.text('content');
@@ -11,13 +11,13 @@ exports.up = function(knex, Promise) {
       table.unique('name');
     }),
     knex.schema.createTable('tags', function(table) {
-      table.increments();
+      table.increments('id');
       table.timestamps(false, true);
       table.string('name');
       table.unique('name');
     }),
     knex.schema.createTable('notes_tags', function(table) {
-      table.increments();
+      table.increments('id');
       table.integer('note_id').unsigned();
       table.integer('tag_id').unsigned();
       table.foreign('note_id').references('notes.id');
